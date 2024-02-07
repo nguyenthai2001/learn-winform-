e724b2fa2d52ae30b102c8605b7208620e778f46
nguyenthai2001 <146408348+nguyenthai2001@users.noreply.github.com>
2024-02-07 12:52:14 +0700

    Add files via upload

---
+++ Camera/WindowsFormsApp17/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApp17
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(24, 56);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(741, 364);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing_1);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Camera/WindowsFormsApp17/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+namespace WindowsFormsApp17
+{
+    public partial class MainForm : Form
+    {
+        private FilterInfoCollection videoDevices; // Danh sách thiết bị video
+        private VideoCaptureDevice videoSource; // Đối tượng chứa video
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox.Image = (System.Drawing.Image)eventArgs.Frame.Clone(); // Hiển thị frame mới lên PictureBox
+        }
+
+        
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice); // Lấy danh sách thiết bị video
+
+            if (videoDevices.Count == 0) // Kiểm tra xem có thiết bị video nào không
+            {
+                MessageBox.Show("Không tìm thấy thiết bị video.");
+                return;
+            }
+
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString); // Sử dụng thiết bị video đầu tiên
+            videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame); // Đăng ký sự kiện khi có frame mới
+            videoSource.Start(); // Bắt đầu khởi động camera
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void MainForm_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning) // Kiểm tra và dừng camera nếu đang chạy
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource = null;
+            }
+        }
+    }
+}
